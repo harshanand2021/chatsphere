b21be87df40a4b3b453da8b815ea99663c155125
Harsh Anand <harsh25061999@gmail.com>
2026-06-29 14:23:36 +0530

    App file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Login from './pages/Login'
-import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import AppShell from './components/AppShell';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import RoomList from './pages/RoomList';
+import CreateRoom from './pages/CreateRoom';
+import HostReady from './pages/HostReady';
+import WatchRoom from './pages/WatchRoom';
 
+export default function App() {
   return (
-      <AuthProvider>
-        <Login />
-      </AuthProvider>
-  )
-}
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
-export default App
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppShell />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/rooms" element={<RoomList />} />
+            <Route path="/host/new" element={<CreateRoom />} />
+            <Route path="/host/:roomId" element={<HostReady />} />
+            <Route path="/room/:roomId" element={<WatchRoom />} />
+          </Route>
+
+          <Route path="/" element={<Navigate to="/rooms" replace />} />
+          <Route path="*" element={<Navigate to="/rooms" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
